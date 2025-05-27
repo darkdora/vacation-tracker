5d904e4ab4912b9fa04f0df09e8fc8cd5df34d45
darkdora <40706544+darkdora@users.noreply.github.com>
2025-05-27 10:42:40 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
